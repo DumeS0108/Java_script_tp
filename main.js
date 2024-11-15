4c53b6f78760987a2b6bac0ca047c4d46cf659f9
SachaD <dumesacha@gmail.com>
2024-11-15 08:44:39 +0100

    khg

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("coucou c'est moi");
+alert("coucou c'est nous");
+var maVariable1 = 12;
+maVariable1 = 13;
+maVariable1 ="toto"
+for (let index = 0; index < 10; index++) {
+    maVariable1 = index;   
+}
+
